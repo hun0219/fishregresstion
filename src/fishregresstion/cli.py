e982bcbfdf73cfb39551e48ea9d8b6896f55d14b
hun0219 <gsw98000@gmail.com>
2024-09-10 00:23:08 +0900

    cli edit

--- src/fishregresstion/cli.py
+++ src/fishregresstion/cli.py
@@ -17,7 +17,7 @@ def lr_api(l, url="http://localhost:8001/lr_fish"):
 
     return r
 
-def knn_api(l, w, n, url="http://localhost:8002/lr_fish"):
+def knn_api(l, w, n_neighbors, url="http://localhost:8002/lr_fish"):
     headers = {
         'accept': 'application/json',
     }
